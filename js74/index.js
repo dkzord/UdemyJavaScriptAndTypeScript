7a03b02f6a62eaf65dfa05f95d87a22200e42fc9
Fernando Cavalcanti(DK) <comododk@gmail.com>
2022-05-16 22:31:51 -0300

    Feat: Factory Function

--- js74/index.js
+++ js74/index.js
@@ -16,4 +16,36 @@
 
 })(27, 95, 1.70);
 
-// 
+// Factory Function
+
+function criaPessoa(nome, sobrenome, altura, peso) {
+  return {
+    nome,
+    sobrenome,
+    //Getter
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+    // Setter
+    set nomeCompleto(valor) {
+      valor = valor.split(' ');
+      this.nome = valor.shift();
+      this.sobrenome = valor.join(' ');
+    },
+    fala(assunto) {
+      return `${nome} esta falando sobre ${assunto}`;
+    },
+    altura,
+    peso,
+    get imc() {
+      const indice = this.peso / (this.altura ** 2);
+      return indice.toFixed(2);
+    }
+  };
+}
+
+const p1 = criaPessoa('Fernando', 'Cavalcanti', 1.70, 95);
+// console.log(p1.fala('Stand Up'));
+// console.log(p1.imc);
+p1.nomeCompleto = 'Marcelino Pão Vinho';
+console.log(p1.nomeCompleto);
