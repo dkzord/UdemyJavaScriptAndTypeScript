d8d6ef70aee6c1334751e2e3dad97fa9ff42c91a
DK <comododk@gmail.com>
2022-07-19 15:21:59 -0300

    fetch e axios

---
+++ js112/assets/js/ajaxXML.js
@@ -0,0 +1,32 @@
+/* fetch('assets/json/pessoas.json')
+  .then(response => response.json())
+  .then(json => carregaElementosPagina(json))
+  .catch(err => console.error(err)) */
+
+axios('assets/json/pessoas.json')
+  .then(response => carregaElementosPagina(response.data))
+
+function carregaElementosPagina(json) {
+  const table = document.createElement('table');
+
+  for (let pessoa of json) {
+    const tr = document.createElement('tr');
+
+    let td = document.createElement('td');
+    td.innerHTML = pessoa.nome
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerHTML = pessoa.idade;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerHTML = pessoa.salario;
+    tr.appendChild(td);
+
+    table.appendChild(tr);
+  }
+
+  const resultado = document.querySelector('.resultado');
+  resultado.appendChild(table);
+}
