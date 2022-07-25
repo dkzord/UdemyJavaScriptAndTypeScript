8af5e25d512e8bec099eae76ca5ad84b772dce99
DK <comododk@gmail.com>
2022-07-25 15:39:55 -0300

    Feat: gerador de cpf

--- js120/src/main.js
+++ js120/src/main.js
@@ -1,3 +1,9 @@
+import GeraCPF from './modules/GeraCPF';
+
 import './assets/css/style.css';
 
-console.log('Deu bom');
+(function () {
+  const gera = new GeraCPF();
+  const cpfGerado = document.querySelector('.cpf-gerado');
+  cpfGerado.innerHTML = gera.geraNovoCPF();
+})();
