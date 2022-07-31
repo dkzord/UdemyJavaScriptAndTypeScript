a78e8b245ccab85058763d348f4bc0f3d98ad708
Fernando Cavalcanti <comododk@gmail.com>
2022-07-31 11:42:13 -0300

    teste git

--- js120/src/main.js
+++ js120/src/main.js
@@ -4,6 +4,7 @@ import './assets/css/style.css';
 
 (function () {
   const gera = new GeraCPF();
+  console.log('teste note');
   const cpfGerado = document.querySelector('.cpf-gerado');
   cpfGerado.innerHTML = gera.geraNovoCPF();
 })();
