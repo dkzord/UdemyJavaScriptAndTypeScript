5c6cfdac2228bb28daed97cfd5d2c4da3fb34f74
Fernando Cavalcanti <comododk@gmail.com>
2022-08-19 10:33:42 -0300

    feat: test creat aluno

--- Node/aula169 - API Rest/src/controllers/HomeController.js
+++ Node/aula169 - API Rest/src/controllers/HomeController.js
@@ -3,12 +3,12 @@
 class HomeController {
   async index(req, res) {
     const novoAluno = await Aluno.create({
-      nome: 'Maria',
-      sobrenome: 'Miranda',
-      email: 'maria@gmail.com',
-      idade: 23,
-      peso: 57,
-      altura: 1.70,
+      nome: 'Fernando',
+      sobrenome: 'Cavalcanti',
+      email: 'fernando@aluno.com',
+      idade: 232,
+      peso: 573,
+      altura: 2.70,
     });
     res.json(novoAluno);
   }
