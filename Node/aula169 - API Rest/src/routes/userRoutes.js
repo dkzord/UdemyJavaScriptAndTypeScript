3c1d44509dc26fd2dc8818e6d8b1f973bd854ad4
Fernando Cavalcanti <comododk@gmail.com>
2022-08-29 14:08:31 -0300

    Feat: structure login user

---
+++ Node/aula169 - API Rest/src/routes/userRoutes.js
@@ -0,0 +1,16 @@
+﻿import { Router } from 'express';
+import userController from '../controllers/UserController';
+
+const router = new Router();
+
+router.post('/', userController.store);
+
+export default router;
+
+/*
+index -> Lista todos os usuários. -> GET
+create/strore -> Cria um novo usuário. -> POST
+delete -> Apaga um usuário. -> DELETE
+show -> Mostra um usuário.  -> GET
+update -> Atualiza um usuário. -> PATCH ou PUT
+*/
