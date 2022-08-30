b90738f82a4fbf51a6dfac03e0febe7bfa405c10
Fernando Cavalcanti <comododk@gmail.com>
2022-08-29 21:51:41 -0300

    Feat: validator toke in middleware

---
+++ Node/aula169 - API Rest/src/middlewares/loginRequired.js
@@ -0,0 +1,25 @@
+﻿import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status('401').json({
+      error: ['Usuário não esta logado'],
+    });
+  }
+
+  const [, token] = authorization.split(' ');
+
+  try {
+    const dados = jwt.verify(token, process.env.TOKEN_SECRET);
+    const { id, email } = dados;
+    req.userId = id;
+    req.userEmail = email;
+    return next();
+  } catch (err) {
+    return res.status('401').json({
+      error: ['Token expirado ou inválido'],
+    });
+  }
+};
