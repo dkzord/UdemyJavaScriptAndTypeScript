b94e95253814fc7c3e6255107514ed6d82b40fa2
Fernando Cavalcanti <comododk@gmail.com>
2022-08-30 07:35:25 -0300

    Feat: create new repository

--- Node/aula169 - API Rest/src/controllers/HomeController.js
+++ Node/aula169 - API Rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-﻿import Aluno from '../models/Aluno';
-
-class HomeController {
+﻿class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Fernando',
-      sobrenome: 'Cavalcanti',
-      email: 'fernando@aluno.com',
-      idade: 232,
-      peso: 573,
-      altura: 2.70,
-    });
-    res.json(novoAluno);
+    res.json('Index da HomeController');
   }
 }
 
