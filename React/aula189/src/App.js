c6ce5696a44a366e43c9f94fcc25f264e7959fd7
Fernando Cavalcanti <comododk@gmail.com>
2022-09-20 01:31:52 -0300

    Feat: structure created and config eslint with prettier

---
+++ React/aula189/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Main from './components/Main';
+
+export default function App() {
+  return (
+    <>
+      <h1>Hellow World!</h1>
+      <p>teste aqui</p>
+      <Main />
+    </>
+  );
+}
---
+++ React/aula189/src/components/Main.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+export default class Main extends Component {
+  state = {
+    newTask: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      newTask: e.target.value,
+    });
+  };
+
+  render() {
+    // const { newTask } = this.state;
+
+    return (
+      <div className="main">
+        <h1>Lista de tarefas</h1>
+        <form action="#">
+          <input type="text" onChange={this.handleChange} />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    );
+  }
+}
+
