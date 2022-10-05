c13f38a77e4889018feb3eee8cbfdd22656da1c2
Fernando Cavalcanti <comododk@gmail.com>
2022-10-04 23:20:52 -0300

    feat: init

--- React/aula189/src/App.js
+++ React/aula189/src/App.js
@@ -5,8 +5,6 @@ import Main from './components/Main';
 export default function App() {
   return (
     <>
-      <h1>Hellow World!</h1>
-      <p>teste aqui</p>
       <Main />
     </>
   );
--- React/aula189/src/components/Main.js
+++ React/aula189/src/components/Main.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+
+/* Form */
+import { FaPlus } from 'react-icons/fa';
+
+/* Tasks */
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     newTask: '',
+    tasks: [],
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tasks } = this.state;
+    let { newTask } = this.state
+    newTask = newTask.trim();
+
+    if (tasks.indexOf(newTask) !== -1) return;
+
+    const newsTasks = [...tasks];
+
+    this.setState({
+      tasks: [...newsTasks, newTask],
+    });
   }
 
   handleChange = (e) => {
@@ -12,16 +36,32 @@ export default class Main extends Component {
     });
   };
 
+
+
   render() {
-    // const { newTask } = this.state;
+    const { newTask, tasks } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de tarefas</h1>
-        <form action="#">
-          <input type="text" onChange={this.handleChange} />
-          <button type="submit">Enviar</button>
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input type="text" onChange={this.handleChange} value={newTask} />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
+
+        <ul className="tasks">
+          {tasks.map(task => (
+            <li key={task}>
+              {task}
+              <span>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
